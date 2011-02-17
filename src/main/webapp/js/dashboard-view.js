2186ba92f342e5c31e50ca289d58b18b6a4e7b0b
Marco Ambu <marco.ambu@gmail.com>
2011-02-17 22:06:19 +0000

    [dashboard-view-plugin] Improved usability for collapse/expand buttons [JENKINS-8624]

--- src/main/webapp/js/dashboard-view.js
+++ src/main/webapp/js/dashboard-view.js
@@ -9,20 +9,45 @@ function getElementFromEvent(e) {
   return targ;
 }
 
+function updateIcons(elemName, param)
+{
+  var cmds = window.document.getElementsByTagName('img');
+  var collapse;
+  var expand;
+  for (var cmdIndex in cmds) {
+    cmd = cmds[cmdIndex];
+    if (cmd.id && cmd.id == ('cmdExp-'+elemName)) {
+      expand = cmd;
+    } else if (cmd.id && cmd.id == ('cmdCol-'+elemName)) {
+      collapse = cmd;
+    }
+  }
+  expand.style.visibility = (param == 'e') ? 'hidden' : 'visible';
+  expand.style.display = (param == 'e') ? 'none' : '';
+  collapse.style.visibility = (param == 'c') ? 'hidden' : 'visible';
+  collapse.style.display = (param == 'c') ? 'none' : '';
+}
+
 function myshow(e) {
   if (!e) var e = window.event;
   var receiver = getElementFromEvent(e);
-  var elem = document.getElementById(receiver.id.substring(7));
+  var portletId = receiver.id.substring(7);
+  var elem = document.getElementById(portletId);
   elem.style.visibility = 'visible';
   elem.style.display = '';
+
+  updateIcons(portletId, 'e');
 }
 
 function myhide(e) {
   if (!e) var e = window.event;
   var receiver = getElementFromEvent(e);
-  var elem = document.getElementById(receiver.id.substring(7));
+  var portletId = receiver.id.substring(7);
+  var elem = document.getElementById(portletId);
   elem.style.visibility = 'hidden';
   elem.style.display = 'none';
+
+  updateIcons(portletId, 'c');
 }
 
 function startsWith(str, substr) {
@@ -34,6 +59,8 @@ for (var cmdIndex in cmds) {
   cmd = cmds[cmdIndex];
   if (cmd.id && startsWith(cmd.id, 'cmdExp-dashboard_portlet_')) {
     cmd.onclick = myshow;
+    cmd.style.visibility = 'hidden';
+    cmd.style.display = 'none';
   } else if (cmd.id && startsWith(cmd.id, 'cmdCol-dashboard_portlet_')) {
     cmd.onclick = myhide;
   }
