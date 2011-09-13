ded1772ece06c7b9eddd2c48b9cb9280eda6a88b
lucinka <lucinka@localhost.localdomain>
2011-09-13 09:49:53 +0200

    new statistics portlet

---
+++ src/main/java/hudson/plugins/view/dashboard/stats/StatCommon.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hudson.plugins.view.dashboard.stats;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.model.Hudson;
+import hudson.plugins.view.dashboard.DashboardPortlet;
+import org.kohsuke.stapler.DataBoundConstructor;
+import hudson.plugins.view.dashboard.Messages;
+import org.kohsuke.stapler.bind.JavaScriptMethod;
+
+/**
+ * 
+ * 
+ * @author Lucie Votypkova
+ */
+public class StatCommon extends DashboardPortlet {
+
+    @DataBoundConstructor
+    public StatCommon(String name) {
+        super(name);
+    }
+
+    @JavaScriptMethod
+    public int getSlaves() {
+        Hudson hudson = Hudson.getInstance();
+        return hudson.getNodes().size();
+    }
+
+    @JavaScriptMethod
+    public int getWorkingSlaves() {
+        Hudson hudson = Hudson.getInstance();
+        return hudson.getNodes().size();
+    }
+
+    @JavaScriptMethod
+    public int getTasksInQueue() {
+        Hudson hudson = Hudson.getInstance();
+        return hudson.getQueue().getItems().length;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends Descriptor<DashboardPortlet> {
+
+        @Override
+        public String getDisplayName() {
+            return Messages.Dashboard_CommonStatistics();
+        }
+    }
+}
