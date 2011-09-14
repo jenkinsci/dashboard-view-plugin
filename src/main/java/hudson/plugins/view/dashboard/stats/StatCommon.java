9efeb54b75100fe4bc092d4f425e8486f1292b55
lucinka <lucinka@localhost.localdomain>
2011-09-14 07:59:17 +0200

    add new functionality

--- src/main/java/hudson/plugins/view/dashboard/stats/StatCommon.java
+++ src/main/java/hudson/plugins/view/dashboard/stats/StatCommon.java
@@ -5,11 +5,15 @@
 package hudson.plugins.view.dashboard.stats;
 
 import hudson.Extension;
+import hudson.model.Computer;
 import hudson.model.Descriptor;
 import hudson.model.Hudson;
+import hudson.model.Job;
+import hudson.model.Node;
 import hudson.plugins.view.dashboard.DashboardPortlet;
 import org.kohsuke.stapler.DataBoundConstructor;
 import hudson.plugins.view.dashboard.Messages;
+import java.util.List;
 import org.kohsuke.stapler.bind.JavaScriptMethod;
 
 /**
@@ -31,15 +35,61 @@ public class StatCommon extends DashboardPortlet {
     }
 
     @JavaScriptMethod
-    public int getWorkingSlaves() {
+    public int getOnlineSlaves() {
         Hudson hudson = Hudson.getInstance();
-        return (hudson.getComputers().length - 1);
+         Computer[] computers = hudson.getComputers();
+        int countOnlineSlaves = 0;
+        for(Computer computer : computers){
+            if(computer.isOnline()){
+                countOnlineSlaves ++;
+            }
+        }
+        return countOnlineSlaves -1;
+    }
+    
+    @JavaScriptMethod
+    public int getOfflineSlaves() {
+        Hudson hudson = Hudson.getInstance();
+        Computer[] computers = hudson.getComputers();
+        int countOfflineSlaves = 0;
+        for(Computer computer : computers){
+            if(computer.isOffline()){
+                countOfflineSlaves ++;
+            }
+        }
+        return countOfflineSlaves;
+    }
+    
+    @JavaScriptMethod
+    public int getDisconnectedSlaves() {
+        Hudson hudson = Hudson.getInstance();
+        Computer[] computers = hudson.getComputers();
+        int countDisconnectedSlaves = 0;
+        for(Computer computer : computers){
+            if(computer.getConnectTime()== 0){
+                countDisconnectedSlaves ++;
+            }
+        }
+        return countDisconnectedSlaves;
     }
 
     @JavaScriptMethod
     public int getTasksInQueue() {
         Hudson hudson = Hudson.getInstance();
         return hudson.getQueue().getItems().length;
+    }
+    
+    @JavaScriptMethod
+    public int getRunningJobs() {
+        Hudson hudson = Hudson.getInstance();
+        List<Job> jobs = hudson.getAllItems(Job.class);
+        int countRunningJobs = 0;
+        for(Job job:jobs){
+            if(job.isBuilding()){
+               countRunningJobs ++; 
+            }
+        }
+        return countRunningJobs;
     }
 
     @Extension
