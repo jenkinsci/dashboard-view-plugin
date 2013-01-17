5867af96962ec04d16ec518ed4fcad7bf9ae391c
syl20bnr <sylvain.benner@gmail.com>
2013-01-17 00:19:32 -0500

    Added configuration to JobsPortlet: Number of column and job filling direction

--- src/main/java/hudson/plugins/view/dashboard/core/JobsPortlet.java
+++ src/main/java/hudson/plugins/view/dashboard/core/JobsPortlet.java
@@ -1,7 +1,11 @@
 package hudson.plugins.view.dashboard.core;
 
+import java.util.List;
+
 import hudson.Extension;
+import hudson.model.TopLevelItem;
 import hudson.model.Descriptor;
+import hudson.model.Job;
 import hudson.plugins.view.dashboard.DashboardPortlet;
 
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -15,9 +19,52 @@ import hudson.plugins.view.dashboard.Messages;
  */
 public class JobsPortlet extends DashboardPortlet {
 
+   private int columnCount = 3;
+   private boolean fillColumnFirst = false;
+
    @DataBoundConstructor
-   public JobsPortlet(String name) {
+   public JobsPortlet(String name,
+                      int columnCount,
+                      boolean fillColumnFirst) {
       super(name);
+      this.columnCount = columnCount;
+      this.fillColumnFirst = fillColumnFirst;
+   }
+   
+   public int getColumnCount() {
+      return this.columnCount <= 0 ? 3 : this.columnCount;
+   }
+
+   public int getRowCount() {
+      int s = this.getDashboard().getJobs().size();
+      int rowCount = s / this.columnCount;
+      if (s % this.columnCount > 0){
+         rowCount += 1;
+      }
+      return rowCount;
+   }
+
+   public boolean getFillColumnFirst() {
+      return this.fillColumnFirst;
+   }
+
+   public Job getJob(int curRow, int curColumun){
+      List<Job> jobs = this.getDashboard().getJobs();
+      int idx = 0;
+      // get grid coordinates from given params
+      if (this.fillColumnFirst){
+         idx = curRow + curColumun * this.getRowCount();
+         if (idx >= jobs.size()){
+            return null;
+         }
+      }
+      else {
+         idx = curColumun + curRow * this.columnCount;
+         if (idx >= jobs.size()){
+            return null;
+         }
+      }
+     return jobs.get(idx);
    }
 
    @Extension
