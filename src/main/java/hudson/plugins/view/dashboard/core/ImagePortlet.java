bc7e6b0fcd7f10996683e4cfad9f5d31a96f98c8
Tobias Gruetzmacher <tobias-git@23.gs>
2019-02-26 00:37:50 +0100

    Fix JENKINS-21048: Image portlet not collapsible
    Thanks to Kyle Paice for the fix.

--- src/main/java/hudson/plugins/view/dashboard/core/ImagePortlet.java
+++ src/main/java/hudson/plugins/view/dashboard/core/ImagePortlet.java
@@ -3,10 +3,8 @@ package hudson.plugins.view.dashboard.core;
 import hudson.Extension;
 import hudson.model.Descriptor;
 import hudson.plugins.view.dashboard.DashboardPortlet;
-
-import org.kohsuke.stapler.DataBoundConstructor;
-
 import hudson.plugins.view.dashboard.Messages;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * Portlet displays image fetched from specified URL
@@ -15,24 +13,24 @@ import hudson.plugins.view.dashboard.Messages;
  */
 public class ImagePortlet extends DashboardPortlet {
 
-    private String url;
+  private String url;
 
-    @DataBoundConstructor
-    public ImagePortlet(String name, String url) {
-        super(name);
-        this.url = url;
+  @DataBoundConstructor
+  public ImagePortlet(String name, String url) {
+    super(name);
+    this.url = url;
+  }
+
+  public String getUrl() {
+    return this.url;
+  }
+
+  @Extension
+  public static class DescriptorImpl extends Descriptor<DashboardPortlet> {
+
+    @Override
+    public String getDisplayName() {
+      return Messages.Dashboard_Image();
     }
-
-    public String getUrl() {
-        return this.url;
-    }
-
-    @Extension
-    public static class DescriptorImpl extends Descriptor<DashboardPortlet> {
-
-        @Override
-        public String getDisplayName() {
-            return Messages.Dashboard_Image();
-        }
-    }
+  }
 }
