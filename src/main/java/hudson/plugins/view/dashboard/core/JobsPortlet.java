a2e527408eeaed855ec45f8060078e522e93bd4b
Tobias Gruetzmacher <tobias-git@23.gs>
2019-02-25 23:29:03 +0100

    Modernize JobsPortlet
    - Replace raw images with icon tags
    - Model build button after BuildButtonColumn from Jenkins core, so that
      it works with and without CSRF protection and for parameterized jobs
    - Sort Jobs by display name (This fixes JENKINS-23644)

--- src/main/java/hudson/plugins/view/dashboard/core/JobsPortlet.java
+++ src/main/java/hudson/plugins/view/dashboard/core/JobsPortlet.java
@@ -1,83 +1,81 @@
 package hudson.plugins.view.dashboard.core;
 
-import java.util.List;
-
+import com.google.common.collect.Lists;
 import hudson.Extension;
 import hudson.model.Descriptor;
 import hudson.model.Job;
 import hudson.plugins.view.dashboard.DashboardPortlet;
-
-import org.kohsuke.stapler.DataBoundConstructor;
-
 import hudson.plugins.view.dashboard.Messages;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * Portlet displays a grid of job names with status and links to jobs.
- * 
+ *
  * @author Peter Hayes
  */
 public class JobsPortlet extends DashboardPortlet {
 
-   private static final int MIN_COLUMNCOUNT = 3;
-   
-   private int columnCount;
-   private boolean fillColumnFirst = false;
+  private static final int MIN_COLUMN_COUNT = 3;
 
-   @DataBoundConstructor
-   public JobsPortlet(String name,
-                      int columnCount,
-                      boolean fillColumnFirst) {
-      super(name);
-      this.columnCount = columnCount;
-      this.fillColumnFirst = fillColumnFirst;
-   }
-   
-   public int getColumnCount() {
-      return columnCount <= 0 ? MIN_COLUMNCOUNT : columnCount;
-   }
+  private int columnCount;
+  private boolean fillColumnFirst = false;
 
-   public int getRowCount() {
-      int s = this.getDashboard().getJobs().size();
-      int rowCount = s / this.getColumnCount();
-      if (s % this.getColumnCount() > 0){
-         rowCount += 1;
-      }
-      return rowCount;
-   }
+  @DataBoundConstructor
+  public JobsPortlet(String name, int columnCount, boolean fillColumnFirst) {
+    super(name);
+    this.columnCount = columnCount;
+    this.fillColumnFirst = fillColumnFirst;
+  }
 
-   public boolean getFillColumnFirst() {
-      return this.fillColumnFirst;
-   }
+  public int getColumnCount() {
+    return columnCount <= 0 ? MIN_COLUMN_COUNT : columnCount;
+  }
 
-   public Job getJob(int curRow, int curColumun){
-      List<Job> jobs = this.getDashboard().getJobs();
-      int idx;
-      // get grid coordinates from given params
-      if (this.fillColumnFirst){
-         idx = curRow + curColumun * this.getRowCount();
-         if (idx >= jobs.size()){
-            return null;
-         }
-      }
-      else {
-         idx = curColumun + curRow * this.getColumnCount();
-         if (idx >= jobs.size()){
-            return null;
-         }
-      }
-     return jobs.get(idx);
-   }
+  public List<List<Job>> getJobs() {
+    List<Job> jobs = this.getDashboard().getJobs();
+    Collections.sort(
+        jobs,
+        new Comparator<Job>() {
+          public int compare(Job p1, Job p2) {
+            return p1.getDisplayName().compareToIgnoreCase(p2.getDisplayName());
+          }
+        });
 
-   @Extension
-   public static class DescriptorImpl extends Descriptor<DashboardPortlet> {
+    if (this.fillColumnFirst) {
+      return transposed(jobs);
+    } else {
+      return Lists.partition(jobs, this.getColumnCount());
+    }
+  }
 
-      public int getDefaultCoulmnCount() {
-          return MIN_COLUMNCOUNT;
-      } 
-      
-      @Override
-      public String getDisplayName() {
-         return Messages.Dashboard_JobsGrid();
-      }
-   }
+  private List<List<Job>> transposed(List<Job> jobs) {
+    int rowCount = (jobs.size() + 1) / this.getColumnCount();
+    List<List<Job>> result = new ArrayList<List<Job>>(rowCount);
+    for (int i = 0; i < rowCount; i++) {
+      result.add(new ArrayList<Job>(this.getColumnCount()));
+    }
+    int c = 0;
+    for (Job job : jobs) {
+      result.get(c).add(job);
+      c = (c + 1) % rowCount;
+    }
+    return result;
+  }
+
+  @Extension
+  public static class DescriptorImpl extends Descriptor<DashboardPortlet> {
+
+    public int getDefaultColumnCount() {
+      return MIN_COLUMN_COUNT;
+    }
+
+    @Override
+    public String getDisplayName() {
+      return Messages.Dashboard_JobsGrid();
+    }
+  }
 }
