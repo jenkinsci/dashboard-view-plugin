d9eb5d47fa668fda7c9960d972ecbbb75b6522df
Olivier Lamy <olamy@apache.org>
2022-02-04 14:20:51 +1000

    [SECURITY-2559] Stored XSS vulnerability iframeportlet
    [SECURITY-2565] add sandbox attribute per default empty with system properties to disable and/or add attribute content
    
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/hudson/plugins/view/dashboard/core/IframePortlet.java
+++ src/main/java/hudson/plugins/view/dashboard/core/IframePortlet.java
@@ -1,23 +1,34 @@
 package hudson.plugins.view.dashboard.core;
 
+import edu.umd.cs.findbugs.annotations.CheckForNull;
 import hudson.Extension;
 import hudson.model.Descriptor;
 import hudson.model.Job;
 import hudson.plugins.view.dashboard.DashboardPortlet;
 import hudson.plugins.view.dashboard.Messages;
+import hudson.util.FormValidation;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Iterator;
+import jenkins.util.SystemProperties;
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
 
 public class IframePortlet extends DashboardPortlet {
 
   private String iframeSource;
   private String effectiveUrl;
   private final String divStyle;
+  private static boolean DO_NOT_USE_SANDBOX =
+      SystemProperties.getBoolean(IframePortlet.class.getName() + ".doNotUseSandbox");
+  private static String SANDBOX_VALUE =
+      SystemProperties.getString(IframePortlet.class.getName() + ".sandboxAttributeValue", "");
 
   @DataBoundConstructor
-  public IframePortlet(String name, String iframeSource, String divStyle) {
+  public IframePortlet(String name, String divStyle) {
     super(name);
-    this.setIframeSource(iframeSource);
     this.divStyle = divStyle;
   }
 
@@ -33,9 +44,22 @@ public class IframePortlet extends DashboardPortlet {
     return divStyle;
   }
 
+  @DataBoundSetter
   public void setIframeSource(String iframeSource) {
     this.iframeSource = iframeSource;
     this.overridePlaceholdersInUrl();
+  }
+
+  public boolean isIframeSourceValid() {
+    return getUrlError(iframeSource) == null;
+  }
+
+  public boolean isUseSandbox() {
+    return !DO_NOT_USE_SANDBOX;
+  }
+
+  public String getSandboxValue() {
+    return SANDBOX_VALUE;
   }
 
   private void overridePlaceholdersInUrl() {
@@ -70,6 +94,37 @@ public class IframePortlet extends DashboardPortlet {
     @Override
     public String getDisplayName() {
       return Messages.Dashboard_IframePortlet();
+    }
+
+    public FormValidation doCheckIframeSource(@QueryParameter String value) {
+      String error = getUrlError(value);
+      if (error != null) {
+        return FormValidation.error(error);
+      }
+      return FormValidation.ok();
+    }
+  }
+
+  /**
+   * Checks if the passed string is a valid HTTP or relative URL.
+   *
+   * @return Localized error message or null if URL is valid.
+   */
+  @CheckForNull
+  protected static final String getUrlError(String url) {
+    if (StringUtils.isBlank(url)) {
+      return Messages.Dashboard_UrlEmpty();
+    }
+    try {
+      final URI allowedUrl = new URI(url);
+      final String protocol = allowedUrl.getScheme();
+      if (!allowedUrl.isAbsolute() || protocol.equals("http") || protocol.equals("https")) {
+        return null;
+      } else {
+        return Messages.Dashboard_UrlHttp();
+      }
+    } catch (URISyntaxException e) {
+      return Messages.Dashboard_UrlInvalid(e.getMessage());
     }
   }
 }
