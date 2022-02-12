1287e31de070c75e09bed77130f8d35a3e059ad3
Olivier Lamy <olamy@apache.org>
2022-02-12 11:53:55 +1000

    SystemProperties limited with old core version
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/hudson/plugins/view/dashboard/core/IframePortlet.java
+++ src/main/java/hudson/plugins/view/dashboard/core/IframePortlet.java
@@ -10,7 +10,6 @@ import hudson.util.FormValidation;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Iterator;
-import jenkins.util.SystemProperties;
 import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
@@ -22,9 +21,9 @@ public class IframePortlet extends DashboardPortlet {
   private String effectiveUrl;
   private final String divStyle;
   private static boolean DO_NOT_USE_SANDBOX =
-      SystemProperties.getBoolean(IframePortlet.class.getName() + ".doNotUseSandbox");
+      Boolean.getBoolean(IframePortlet.class.getName() + ".doNotUseSandbox");
   private static String SANDBOX_VALUE =
-      SystemProperties.getString(IframePortlet.class.getName() + ".sandboxAttributeValue", "");
+      System.getProperty(IframePortlet.class.getName() + ".sandboxAttributeValue", "");
 
   @DataBoundConstructor
   public IframePortlet(String name, String divStyle) {
